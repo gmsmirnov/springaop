a48d7486508b927e023d03f79a0f244a0bd69a1f
gmsmirnov <artress@ngs.ru>
2019-06-28 18:08:45 +0700

    AOP

--- src/test/java/ru/gsmirnov/springapp/models/InstrumentalistTest.java
+++ src/test/java/ru/gsmirnov/springapp/models/InstrumentalistTest.java
@@ -2,23 +2,13 @@ package ru.gsmirnov.springapp.models;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration( {"spring-config.xml"})
+import static org.junit.Assert.*;
+
 public class InstrumentalistTest {
-    @Autowired
-    Instrumentalist eddie;
-
-    @Autowired
-    Instrument guitar;
-
-//    ApplicationContext context = new ClassPathXmlApplicationContext("ru.gsmirnov.springapp.models/spring-config.xml");
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 
     @Before
     public void init() {
@@ -27,7 +17,7 @@ public class InstrumentalistTest {
 
     @Test
     public void test() {
-//        Instrumentalist eddie = this.context.getBean(Instrumentalist.class);
+        Instrumentalist eddie = this.context.getBean(Instrumentalist.class);
         eddie.perform();
     }
 }
